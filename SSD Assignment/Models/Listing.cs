6201c02d35be7f0a815a358c5b81ffb928b8c0fc
Ng Wei Han Ryan <s10163108@connect.np.edu.sg>
2018-07-16 22:27:57 +0800

    Created Listing Model

---
+++ SSD Assignment/Models/Listing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSDAssignment.Models
+{
+    public class Listing
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Condition { get; set; }
+    }
+}
