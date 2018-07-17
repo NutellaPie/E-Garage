742f6d9e20e49f61dd04364c07a7f7e71f526d7d
Ng Wei Han Ryan <s10163108@connect.np.edu.sg>
2018-07-17 23:54:00 +0800

    Added Listing Photos Upload

---
+++ SSD Assignment/Data/Migrations/20180717152632_Listingphoto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SSD_Assignment.Data.Migrations
+{
+    public partial class Listingphoto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoPath",
+                table: "Listing",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoPath",
+                table: "Listing");
+        }
+    }
+}
--- SSD Assignment/Models/Listing.cs
+++ SSD Assignment/Models/Listing.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,5 +16,9 @@ namespace SSD_Assignment.Models
         public string Description { get; set; }
         public string Category { get; set; }
         public string Condition { get; set; }
+        public string PhotoPath { get; set; }
+        [Required]
+        [NotMapped]
+        public IFormFile Photo { get; set; }
     }
 }
